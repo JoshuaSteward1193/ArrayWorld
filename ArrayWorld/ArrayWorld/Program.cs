92e07f36caad256618c69ad2ea1f18edf067efe0
Joshua Steward <joshua.steward@students.fhu.edu>
2018-04-20 21:59:24 -0500

    Init

---
+++ ArrayWorld/ArrayWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayWorld
+{
+    class Program
+    {
+        public static Room[,] world = new Room[10, 10];
+        static void Main(string[] args)
+        {                        
+            for(int i = 0; i <= world.Length; i++)
+            {
+                for(int j = 0; j < world.Length - 1; j++)
+                {
+                    world[i, j] = new Room();
+                }
+            }
+            PrintAllRooms();
+            Console.ReadKey();
+        }
+        private static void PrintAllRooms()
+        {
+            for (int i = 0; i < world.Length; i++)
+            {
+                for (int j = 0; j <= world.Length; j++)
+                {
+                    world[i, j].PrintRoom();
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ ArrayWorld/ArrayWorld/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayWorld
+{
+    class Room
+    {
+        public string Description { get; set; }
+        public List<Room> levels = new List<Room>();
+
+        public Room()
+        {
+            Description = "There is nothing here. This part of the world is empty.";            
+        }
+        public void PrintRoom()
+        {
+            Console.Write($"{Description} | ");
+        }
+    }
+}
